226d9fa96dd2f481e204e77fb28761363a557e8b
MyLovingOrange <38748076+MyLovingOrange@users.noreply.github.com>
2019-11-16 16:53:44 +0800

    添加requirement

--- user/views.py
+++ user/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render,render_to_response,redirect
-import simplejson
-import time
 import Crypto.Hash.SHA256 as SHA256
 from django.urls import reverse
 def index(request):
